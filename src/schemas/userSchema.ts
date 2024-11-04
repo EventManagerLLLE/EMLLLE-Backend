a40ef622cdb0b52f55fe6921c2cf38bbe6155dd3
Luka Arsenijevic <luka.arsenijevic@husqvarnagroup.com>
2024-11-04 10:42:31 +0100

    removed passwordRepeat

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,17 +1,11 @@
 import { z } from 'zod';
 
-export const userSchema = z
-  .object({
-    id: z.string().uuid().optional(),
-    username: z.string().min(3),
-    firstName: z.string().min(3),
-    lastName: z.string().min(3),
-    password: z.string().min(6),
-    repeatPassword: z.string().min(6),
-  })
-  .refine((data) => data.password === data.repeatPassword, {
-    message: "Passwords don't match",
-    path: ['repeatPassword'], // This will show the error at the repeatPassword field
-  });
+export const userSchema = z.object({
+  id: z.string().uuid().optional(),
+  username: z.string().min(3),
+  firstName: z.string().min(3),
+  lastName: z.string().min(3),
+  password: z.string().min(6),
+});
 
 export type User = z.infer<typeof userSchema>;
